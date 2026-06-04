e45c99eb70c888ed0081fe49aa128f83a55c524a
Aniket Zaware <87866953+Anichetu@users.noreply.github.com>
2026-06-04 20:14:26 +0530

    Update api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,7 +1,7 @@
 // =====================
 // API Configuration
 // =====================
-const API_BASE = 'http://localhost:8080/api';
+const API_BASE = 'https://canteen-management-system.up.railway.app:8080/api';
 
 const API = {
   // Customers
